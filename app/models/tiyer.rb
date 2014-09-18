0244058af2d0d3b4162b90d879b6ed2a37fa45e9
Jim Rambow <jimrambow@gmail.com>
2014-09-18 11:45:25 -0400

    added validation to name first and last, github, and tiy_type

--- app/models/tiyer.rb
+++ app/models/tiyer.rb
@@ -1,3 +1,11 @@
 class Tiyer < ActiveRecord::Base
+
+
       has_attached_file :image
+      validates :first_name, presence: true
+      validates :last_name, presence: true
+      validates :github, presence: true
+      validates :tiy_type, presence: true
+
+
 end
