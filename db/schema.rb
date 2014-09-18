174f9bb2a092b25c461f2a805ef594212012a617
Jim Rambow <jimrambow@gmail.com>
2014-09-18 11:12:48 -0400

    added the paperclip gem and dependencies for it.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917183854) do
+ActiveRecord::Schema.define(version: 20140918150026) do
 
   create_table "tiyers", force: true do |t|
     t.string   "first_name"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20140917183854) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "tiy_type"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: true do |t|
